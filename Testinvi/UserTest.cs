c4ffd6116289d98acb074a2b18e185c3a7b35285
Yossi Tokash <Yossi.Tokash@gmail.com>
2013-12-19 15:15:32 +0200

    Adding to source control
    Adding to source control

---
+++ Testinvi/UserTest.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TweetinCore.Interfaces;
+using Tweetinvi;
+
+namespace Testinvi
+{
+    [TestClass]
+    public class UserTest
+    {
+        #region Constructor
+        [TestMethod]
+        [TestCategory("Constructor"), TestCategory("User")]
+        public void User_Constructor1()
+        {
+            long id = -1;
+            IUser user = new User(id);
+
+            Assert.AreEqual(user.Name, null);
+        }
+
+        [TestMethod]
+        [TestCategory("Constructor"), TestCategory("User")]
+        public void User_Constructor2()
+        {
+            // Expect the Ladygaga to be retrieved from Twitter
+            IUser user = new User("ladygaga", TokenSingleton.Instance);
+
+            Assert.AreEqual(user.Id, 14230524);
+        }
+
+        [TestMethod]
+        [TestCategory("Constructor"), TestCategory("User")]
+        public void User_Constructor3()
+        {
+            // Expect the Ladygaga to be retrieved from Twitter
+            IUser user = new User(14230524, TokenSingleton.Instance);
+
+            Assert.AreEqual(user.ScreenName, "ladygaga");
+        } 
+        #endregion
+
+        #region GetFriends
+
+        [TestMethod]
+        [TestCategory("GetFriends"), TestCategory("User")]
+        public void UserGetFriendIds()
+        {
+            IUser u = new User("linviStevens", TokenSingleton.Instance);
+            List<long> friendsIds = u.GetFriendIds(true);
+            List<IUser> friends = u.Friends;
+
+            Assert.AreNotEqual(friendsIds.Count, 0);
+            Assert.AreEqual(friendsIds.Count, friends.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("GetFriends"), TestCategory("User")]
+        public void UserGetFriendIds1()
+        {
+            IUser u = new User("linviStevens", TokenSingleton.Instance);
+            List<long> friendsIds = u.GetFriendIds(false);
+            List<IUser> friends = u.Friends;
+
+            Assert.AreNotEqual(friendsIds.Count, 0);
+            Assert.AreEqual(friends.Count, 0);
+        }
+
+        [TestMethod]
+        [TestCategory("GetFriends"), TestCategory("User")]
+        public void UserGetFriends()
+        {
+            TokenUser u = new TokenUser(TokenSingleton.Instance);
+
+            u.GetFriendIds(u.ObjectToken);
+            List<long> friendIds = u.GetFriendIds(u.ObjectToken);
+
+            List<IUser> friends = UserUtils.Lookup(friendIds, null, u.ObjectToken);
+
+            Assert.AreEqual(friendIds.Count, friends.Count);
+        }
+
+        #endregion
+
+        #region GetFollowers
+
+        [TestMethod]
+        [TestCategory("GetFollowers"), TestCategory("User")]
+        public void UserGetFollowers()
+        {
+            IUser u = new User("linviStevens", TokenSingleton.Instance);
+            List<long> followersIds = u.GetFollowerIds(true);
+            List<IUser> followers = u.Followers;
+
+            Assert.AreNotEqual(followersIds.Count, 0);
+            Assert.AreEqual(followersIds.Count, followers.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("GetFollowers"), TestCategory("User")]
+        public void UserGetFollowers1()
+        {
+            IUser u = new User("linviStevens", TokenSingleton.Instance);
+            List<long> followersIds = u.GetFollowerIds(false);
+            List<IUser> followers = u.Followers;
+
+            Assert.AreNotEqual(followersIds.Count, 0);
+            Assert.AreEqual(followers.Count, 0);
+        }
+        #endregion
+
+        #region DownloadProfileImage
+
+        [TestMethod]
+        [TestCategory("Image"), TestCategory("User")]
+        public void UserDownloadProfileImage()
+        {
+            Debug.WriteLine(Directory.GetCurrentDirectory());
+            
+            string userName = "ladygaga";
+
+            if (File.Exists(string.Format("{0}_normal.jpg", userName)))
+            {
+                File.Delete(string.Format("{0}_normal.jpg", userName));
+            }
+
+            IUser u = new User(userName, TokenSingleton.Instance);
+            string filepath = u.DownloadProfileImage();
+
+            Assert.AreEqual(filepath, string.Format("{0}_normal.jpg", userName));
+
+            Assert.AreNotEqual(filepath, string.Empty);
+
+            bool fileExist = File.Exists(filepath);
+            Assert.AreEqual(fileExist, true);
+        }
+
+        #endregion
+
+        #region GetContributors
+
+        [TestMethod]
+        [TestCategory("GetContributors"), TestCategory("User")]
+        public void UserGetContributors()
+        {
+            IUser u = new User("ladygaga", TokenSingleton.Instance);
+            List<IUser> contributors = u.GetContributors(true);
+
+            Assert.AreNotEqual(contributors, null);
+            Assert.AreEqual(contributors.Count, 0);
+        }
+
+        #endregion
+
+        #region GetContributees
+        
+        [TestMethod]
+        [TestCategory("GetContributees"), TestCategory("User")]
+        public void UserGetContributees()
+        {
+            IUser u = new User("ladygaga", TokenSingleton.Instance);
+            List<IUser> contributees = u.GetContributees(true);
+
+            Assert.AreNotEqual(contributees, null);
+            Assert.AreEqual(contributees.Count, 0);
+        }
+
+        #endregion
+
+        #region UserTimeline
+
+        [TestMethod]
+        [TestCategory("User Timeline"), TestCategory("User")]
+        public void UserGetTimeline()
+        {
+            IUser u = new User("ladygaga", TokenSingleton.Instance);
+            List<ITweet> tweets = u.GetUserTimeline();
+
+            Assert.AreNotEqual(tweets, null);
+            Assert.AreNotEqual(tweets.Count, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ Tweetinvi/UserUtils.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TweetinCore.Events;
+using TweetinCore.Interfaces;
+using Tweetinvi.Properties;
+
+namespace Tweetinvi
+{
+    /// <summary>
+    /// Access to methods related with user that cannot find
+    /// their place in User or TokenUser
+    /// </summary>
+    public static class UserUtils
+    {
+        #region Private methods
+        
+        /// <summary>
+        /// Update the current query to create the expected query
+        /// </summary>
+        private static string EnrichLookupQuery(string query, string extension)
+        {
+            if (extension.EndsWith("%2C"))
+            {
+                return query + "&" + extension.Remove(extension.Length - 3);
+            }
+            return query;
+        }
+        
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Return a list of users corresponding to the list of user ids and screen names given in parameter.
+        /// Throw an exception if the token given in parameter is null or if both lists given in parameters are null.
+        /// </summary>
+        /// <param name="userIds">
+        ///     List of user screen names. This parameter can be null
+        ///     List of user ids. This parameter can be null
+        /// </param>
+        /// <param name="screenNames"></param>
+        /// <param name="token">Token used to request the users' information from the Twitter API</param>
+        /// <returns>The list of users retrieved from the Twitter API</returns>
+        public static List<IUser> Lookup(List<long> userIds, List<string> screenNames, IToken token)
+        {
+            if (token == null)
+            {
+                throw new ArgumentException("Token must not be null");
+            }
+
+            if (userIds == null && screenNames == null)
+            {
+                throw new ArgumentException("User ids or screen names must be specified");
+            }
+
+            // Maximum number of users that can be requested from the Twitter API (in 1 single request)
+            const int listMaxSize = 100;
+
+            List<IUser> users = new List<IUser>();
+
+            if (userIds == null)
+            {
+                userIds = new List<long>();
+            }
+            if (screenNames == null)
+            {
+                screenNames = new List<string>();
+            }
+
+            int userIndex = 0;
+            int screenNameIndex = 0;
+            while ((userIndex < userIds.Count) || (screenNameIndex < screenNames.Count))
+            {
+                // Keep track of the number of users that we are going to request from the Twitter API
+                int indicesSumBeforeLoop = userIndex + screenNameIndex;
+                string userIdsStrList = "user_id=";
+                string screenNamesStrList = "screen_name=";
+                
+                // Take request parameters from both names list and id list
+                // userIndex + screenNameIndex - indicesSumBeforeLoop) < listMaxSize ==> Check that the number of parameters given to the Twitter API request does not exceed the limit
+                while (((userIndex + screenNameIndex - indicesSumBeforeLoop) < listMaxSize)
+                    && (userIndex < userIds.Count)
+                    && (screenNameIndex < screenNames.Count))
+                {
+                    screenNamesStrList += screenNames.ElementAt(screenNameIndex++) + "%2C";
+                    userIdsStrList += userIds.ElementAt(userIndex++) + "%2C";
+                }
+                // Take request from id list
+                while (((userIndex + screenNameIndex - indicesSumBeforeLoop) < listMaxSize)
+                    && (userIndex < userIds.Count))
+                {
+                    userIdsStrList += userIds.ElementAt(userIndex++) + "%2C";
+                }
+
+                // Take name from name list
+                while (((userIndex + screenNameIndex - indicesSumBeforeLoop) < listMaxSize)
+                    && (screenNameIndex < screenNames.Count))
+                {
+                    screenNamesStrList += screenNames.ElementAt(screenNameIndex++) + "%2C";
+                }
+
+                String query = Resources.UserUtils_Lookup;
+                // Add new parameters to the query and format it
+                query = EnrichLookupQuery(query, screenNamesStrList);
+                query = EnrichLookupQuery(query, userIdsStrList);
+
+                ObjectResponseDelegate objectDelegate = delegate(Dictionary<string, object> responseObject)
+                    {
+                        User u = User.Create(responseObject);
+                        users.Add(u);
+                    };
+
+                token.ExecuteGETQuery(query, objectDelegate);
+            }
+
+            return users;
+        }
+        #endregion
+    }
+}
